b45d8510b44e7ebb942c053adc85fd53193b8499
hseniht <thinesh_kumar_mu@hotmail.com>
2020-10-05 02:07:14 +0800

    Minor Touch ups - added comment notes - rename, delete variables - added condition to check uneven distribution of cards (commented out for now) - fix on css styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 
-//init states
+//initial card values
 const cardsArr = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K"];
 
+//function to map suite to card sets
 const mapSuitsWithCards = (suite, cards) => {
    return cards.map(card => suite + "-" + card);
 }
@@ -12,8 +13,10 @@ const hearts = mapSuitsWithCards("H", cardsArr);
 const diamonds = mapSuitsWithCards("D", cardsArr);
 const club = mapSuitsWithCards("C", cardsArr);
 
+//deck with all suite sets combined
 const deck = [].concat(spades, hearts, diamonds, club)
 
+//function to shuffle deck
 const shuffleCards = (arr) => {
    let array = [...arr]; //clone array
    let m = array.length, temp, i;
@@ -27,6 +30,9 @@ const shuffleCards = (arr) => {
    return array;
 }
 
+const MSG_IRREGULARITY =  "Irregularity occured! Cards can't be distributed fairly among players from the input keyed."
+const MSG_RETRY = "Please try a different value. Aborting.."
+//component for card app
 export default class Dashboard extends Component {
    state = {
       cardsArr: cardsArr,
@@ -38,11 +44,16 @@ export default class Dashboard extends Component {
       // collections: null
    }
 
-   generatePeopleArray = () => {
+   handleCardDistribution = () => {
       const s = this.state;
       let arr = [], inputt = s.inputt;
       let person = {}, rounds;
-      let newDeck = [...s.shuffledDeck]
+      let newDeck = [...s.shuffledDeck] //clone as copy
+      //reset irregularity (not used)
+      // this.setState({
+      //    irregularity: false
+      // })
+
       if (inputt <= 0) {
          return alert("Nobody to distribute to :(");
       } else {
@@ -60,9 +71,22 @@ export default class Dashboard extends Component {
       while (newDeck.length > 0) {
          //cards out per round based on no of person(input)
          rounds = newDeck.splice(0, inputt);
-         //map that cards to persons. 
+         //map that cards to persons
          for (let k = 1; k <= inputt; k++) {
-            person[k].push(rounds[k - 1]); // "-1" to follow index
+            person[k].push(rounds[k - 1]); // offset "-1" to follow array index which starts at "0"
+            
+            //there may not be enough cards to complete a distribution round, giving undefined value to array element
+            //so we can set condition for a message
+            //but uncommenting this out for now as not including it.
+            // if(!rounds[k - 1] || inputt.length >=3) {
+            //    alert(MSG_IRREGULARITY);
+            //    alert(MSG_RETRY);
+            //    this.setState({
+            //       irregularity: true,
+            //       inputt: ""
+            //    })
+            //    return this.reShuffleDeck();
+            // }
          }
 
       }
@@ -70,23 +94,22 @@ export default class Dashboard extends Component {
          collections: person
       })
    }
-   getInput = (e) => {
-      // const input = parseInt(e.target.value, 10);
-      const input = e.target.value;
 
-      //validation run only when there is input value
-      // if(!Number(input) && input.length > 0){
+   getInput = (e) => {
+      const input = e.target.value;
+      //validation for only number (decimal not allowed)
       if(isNaN(input) || input.indexOf(".") !== -1){
          this.setState({
             inputt: "",
          })
-         return alert("“Input value does not exist or value is invalid")
+         return alert("Input value does not exist or value is invalid")
       }
 
       this.setState({
          inputt: input,
       })
    }
+
    reShuffleDeck = () => {
       this.setState({
          shuffledDeck: shuffleCards(deck),
@@ -94,6 +117,7 @@ export default class Dashboard extends Component {
       })
       console.log("deck shuffeld");
    }
+
    render() {
       const { cardsArr, people, inputt, collections } = this.state;
 
@@ -117,7 +141,7 @@ export default class Dashboard extends Component {
             <h5 className="input-label">Enter number of people</h5>
             <input className="input-field" placeholder="Numbers e.g. 4" onChange={this.getInput} value={inputt} />
             <br />
-            <button className="btn-custom deal" type="button" onClick={this.generatePeopleArray}>Deal</button>
+            <button className="btn-custom deal" type="button" onClick={this.handleCardDistribution}>Deal</button>
             <button className="btn-custom reset" type="button" onClick={this.reShuffleDeck}>Reshuffle</button>
             {collections &&
                <div className="list-wrapper">
